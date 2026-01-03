9444d5f225b35600c4a46e33b931ebb964ed553c
Nethmika4881 <nethmikakumarasiri@gmail.com>
2026-01-04 04:41:01 +0530

    changed

--- app/(app)/dashboard/_components/UpcomingFeedsList.jsx
+++ app/(app)/dashboard/_components/UpcomingFeedsList.jsx
@@ -3,7 +3,7 @@ import UpcomingFeedsCard from "./UpcomingFeedsCard";
 function UpcomingFeedsList({ upcomingFeeds }) {
   console.log("upcoming", upcomingFeeds);
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex h-96 flex-col gap-4">
       {upcomingFeeds.map((feed) => (
         <UpcomingFeedsCard
           key={feed.id}
